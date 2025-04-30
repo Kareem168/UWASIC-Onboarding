2263d388b4c599fb66e09ac160f727394eea1c71
Kareem Fahmi <60073461+Kareem168@users.noreply.github.com>
2025-04-30 10:27:39 -0400

    initial commit

--- src/project.v
+++ src/project.v
@@ -5,7 +5,8 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_uwasic_onboarding_kareem_fahmi (
+	assign uio_oe = 8'hFF;		// Set all IOs to output
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -19,9 +20,8 @@ module tt_um_example (
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
-  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, ui_in[7:3], uio_in, clk, rst_n, 1'b0};
 
 endmodule
